78d144ca1da7eac34221616599314c6ef9940260
RoboNex <bilawalrajpoot946@gmail.com>
2026-07-04 21:55:18 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Layouts/Header"; 
 import Footer from "./components/Layouts/Footer"; 
 import Home from "./pages/Home/Home";
